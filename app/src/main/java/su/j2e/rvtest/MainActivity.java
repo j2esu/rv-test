1894ff7df5c9c9a80a569b98a952d33f14b56b4e
Evgeny Egorov <j2esu@ya.ru>
2016-01-19 16:15:24 +0300

    Move swipe handling to ViewHolder

--- app/src/main/java/su/j2e/rvtest/MainActivity.java
+++ app/src/main/java/su/j2e/rvtest/MainActivity.java
@@ -1,45 +1,20 @@
 package su.j2e.rvtest;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements SwipeTouchListener.Callback {
-
-	private static final String TAG = MainActivity.class.getName();
+public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
-		rv.setAdapter(new MyAdapter());
-		rv.addOnItemTouchListener(new SwipeTouchListener(this, this));
+		MyAdapter adapter = new MyAdapter();
+		rv.setAdapter(adapter);
+		rv.addOnItemTouchListener(new SwipeTouchListener(this, adapter));
 		rv.addItemDecoration(new GroupingItemDecoration());
 	}
 
-	@Override
-	public void onDragSwipe(View view, float dx) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			view.setTranslationX(dx);
-		}
-	}
-
-	@Override
-	public void onFlingSwipe(View view, float dx, float vx) {
-		if (vx > 5000) {
-			view.animate().translationX(view.getWidth()).setDuration(1000).start();
-		} else if (vx < -5000) {
-			view.animate().translationX(-view.getWidth()).setDuration(1000).start();
-		} else {
-			view.animate().translationX(0).setDuration(1000).start();
-		}
-	}
-
-	@Override
-	public void onCancelSwipe(View view, float dx) {
-
-	}
 }
--- app/src/main/java/su/j2e/rvtest/MyAdapter.java
+++ app/src/main/java/su/j2e/rvtest/MyAdapter.java
@@ -1,22 +1,31 @@
 package su.j2e.rvtest;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyVh> {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyVh> implements SwipeTouchListener.Callback {
 
 	private static final String TAG = MyAdapter.class.getName();
+
 	private String[] data = new String[20];
 	{
 		for (int i = 0; i < data.length; i++) {
 			data[i] = "Item " + i;
 		}
 	}
+
+	private int animCancelDuration = 300;
+	private int minSwipeVelocity = 4000;
+	private int avgSwipeVelocity = 10000;
+	private int avgSwipeAnimDuration = 100;
+
+	private RecyclerView mRecyclerView;
 
 	@Override
 	public MyVh onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -33,14 +42,44 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyVh> {
 		return data.length;
 	}
 
-	class MyVh extends RecyclerView.ViewHolder implements View.OnTouchListener {
+	@Override
+	public void onDragSwipe(View view, float dx) {
+		((MyVh) mRecyclerView.getChildViewHolder(view)).onDragSwipe(dx);
+	}
+
+	@Override
+	public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+		mRecyclerView = recyclerView;
+	}
+
+	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+	@Override
+	public void onFlingSwipe(View view, float dx, float vx) {
+
+		if (Math.abs(vx) > minSwipeVelocity) {
+			int direction = vx > 0 ? 1 : -1;
+			float duration = avgSwipeAnimDuration * avgSwipeVelocity / Math.abs(vx);
+			view.animate().translationX(direction * view.getWidth())
+					.setDuration((long) duration).start();
+		} else {
+			view.animate().translationX(0).setDuration(animCancelDuration).start();
+		}
+	}
+
+	@Override
+	public void onCancelSwipe(View view, float dx) {
+
+	}
+
+	class MyVh extends RecyclerView.ViewHolder {
 
 		private final TextView text;
+		private final View topView;
 
 		private MyVh(ViewGroup parent) {
 			super(LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_item, parent, false));
 			text = (TextView) itemView.findViewById(R.id.rv_item_text);
-			initSwipeListener();
+			topView = itemView.findViewById(R.id.rv_item_top_view);
 			itemView.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
@@ -53,17 +92,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyVh> {
 			text.setText(s);
 		}
 
-		private void initSwipeListener() {
-
-//			itemView.setOnTouchListener(this);
+		private void onDragSwipe(float dx) {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+				topView.setTranslationX(dx);
+			}
 		}
 
-
-		@Override
-		public boolean onTouch(View v, MotionEvent event) {
-//			Log.d(TAG, "onTouch: ");
-			return true;
-		}
 	}
 
 }
