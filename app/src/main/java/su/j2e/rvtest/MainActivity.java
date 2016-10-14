f6531e9a062fedc022cbe45dcc1cbc6a58736a46
Evgeny Egorov <j2esu@ya.ru>
2016-10-14 10:08:39 +0400

    Test smth.

--- app/src/main/java/su/j2e/rvtest/MainActivity.java
+++ app/src/main/java/su/j2e/rvtest/MainActivity.java
@@ -6,15 +6,20 @@ import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
-		MyAdapter adapter = new MyAdapter();
-		rv.setAdapter(adapter);
-		rv.addOnItemTouchListener(new SwipeTouchListener(this, adapter));
-		rv.addItemDecoration(new GroupingItemDecoration());
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyAdapter adapter = new MyAdapter();
+        RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
+//        rv.setNestedScrollingEnabled(false);
+        rv.setAdapter(adapter);
+//        rv.addOnItemTouchListener(new SwipeTouchListener(this, adapter));
+//        rv.addItemDecoration(new GroupingItemDecoration());
+
+        RecyclerView rv2 = (RecyclerView) findViewById(R.id.rv2);
+        rv2.setAdapter(adapter);
+    }
 
 }
